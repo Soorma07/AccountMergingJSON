2ede45d19ff886450b044a8d929a91b2cfb62189
Adnaan Soorma <Soorma07@gmail.com>
2019-04-08 01:53:55 -0500

    Working final

---
+++ Person.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace AccountMerging
+{
+    // A person can have accounts with multiple emails and multiple applications.
+    public class Person
+    {
+        public List<string> applications;
+        public List<string> emails;
+        public string name;
+    }
+}
