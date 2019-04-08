b6d201d1009bf73ad2a287970c48bc56dbf42676
Adnaan Soorma <Soorma07@gmail.com>
2019-04-08 00:22:53 -0500

    Deserialized JSON

---
+++ Account.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace AccountMerging
+{
+    public class Account
+    {
+        public string Application;
+        public List<string> Emails;
+        public string Name;
+    }
+}
